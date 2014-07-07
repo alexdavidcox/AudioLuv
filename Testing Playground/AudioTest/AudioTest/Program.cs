af991a2be5fef833501b92d906f507fd03ef843f
alex <acox@student.neumont.edu>
2014-07-06 21:55:38 -0600

    Initial Setup

---
+++ Testing Playground/AudioTest/AudioTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.DirectX.DirectSound;
+
+namespace AudioTest
+{
+    class Program
+    {
+
+
+        public static void  Main(String[] args)
+        {
+            Capture capture = new Capture();
+            CaptureBufferDescription desc = new CaptureBufferDescription();
+            CaptureBuffer buffer = new CaptureBuffer(desc, capture);
+            Console.WriteLine(buffer.Caps.BufferBytes);
+            Console.ReadKey();
+        }
+    }
+}
